ee947bb244822ea1a1cd93e0f8dffba9f9f069e1
dingxiaocheng <dingxiaocheng@shinezone.com>
2018-01-30 20:01:24 +0800

    [CODE] temp commit

--- Hunter/Hunter/TexasHoldem/TexasHoldem.cs
+++ Hunter/Hunter/TexasHoldem/TexasHoldem.cs
@@ -15,93 +15,7 @@ namespace Hunter
         public Card card2;
         public int iWinRate;
     }
-
-    public class Color
-    {
-        public const int Spade = 1;
-        public const int Heart = 2;
-        public const int Club = 3;
-        public const int Diamond = 4;
-        public const int Count = 4;
-    }
-
-    public class Card
-    {
-        public static int iTotalCount = 52;
-        public int id;
-        public int iColor;
-        public int iNumber;
-        public bool bUsed;
-        public int GetId()
-        {
-            return (iColor - 1) * 13 + iNumber;
-        }
-        public static int CountId(int iColor, int iNumber)
-        {
-            return (iColor - 1) * 13 + iNumber;
-        }
-        public string Print()
-        {
-            string sColor = "";
-            string sNumber = "";
-            string ret = "";
-            switch (iColor)
-            {
-                case Color.Spade:
-                    sColor = "S";
-                    break;
-                case Color.Heart:
-                    sColor = "H";
-                    break;
-                case Color.Club:
-                    sColor = "C";
-                    break;
-                case Color.Diamond:
-                    sColor = "D";
-                    break;
-                default:
-                    break;
-            }
-            switch (iNumber)
-            {
-                case 1:
-                    sNumber = "A";
-                    break;
-                case 10:
-                    sNumber = "T";
-                    break;
-                case 11:
-                    sNumber = "J";
-                    break;
-                case 12:
-                    sNumber = "Q";
-                    break;
-                case 13:
-                    sNumber = "K";
-                    break;
-                default:
-                    sNumber = Convert.ToString(iNumber);
-                    break;
-            }
-            ret = sColor + sNumber;
-            return ret;
-        }
-    }
-
-    public class CardType
-    {
-        public static int Count = 100;
-        public static int StraightFlush = Count--;
-        public static int FourKind = Count--;
-        public static int FullHouse = Count--;
-        public static int Flush = Count--;
-        public static int Straight = Count--;
-        public static int ThreeKind = Count--;
-        public static int TwoPairs = Count--;
-        public static int OnePairs = Count--;
-        public static int HighCard = Count--;
-    }
-
+    
     public class HandRateStatistics
     {
         public List<HandRateData> DataList;
@@ -130,7 +44,6 @@ namespace Hunter
         public static SortedList<int, Result> ResultList = new SortedList<int, Result>();
         public static string sResult = "";
         
-
         public static SortedList<int, Card> GetAllCardList()
         {
             if (AllCardList.Count != 0)
@@ -181,13 +94,6 @@ namespace Hunter
 
             if (iNum == 1)
             {
-                /*
-                if (preList == null)
-                {
-                    Logger.Error("preList == null...");
-                    return;
-                }
-                */
                 Debug.Assert(retList != null);
 
                 foreach (var item in poolList)
@@ -209,14 +115,6 @@ namespace Hunter
             {
                 next_poolList.Add(item);
             }
-
-            /*
-            List<Card> next_retList = new List<Card>();
-            foreach (var item in retList)
-            {
-                next_retList.Add(item);
-            }
-            */
 
             foreach (var item in poolList)
             {
@@ -274,6 +172,128 @@ namespace Hunter
             return REqual;
         }
 
+        /*
+        public static int Count = 100;
+        public static int StraightFlush = Count--;
+        public static int FourKind = Count--;
+        public static int FullHouse = Count--;
+        public static int Flush = Count--;
+        public static int Straight = Count--;
+        public static int ThreeKind = Count--;
+        public static int TwoPairs = Count--;
+        public static int OnePairs = Count--;
+        public static int HighCard = Count--;
+        */
+        public static bool IsStraight(List<Card> cardList)
+        {
+            return false;
+        }
+        public static bool IsFlush(List<Card> cardList)
+        {
+            return false;
+        }
+        public static bool IsStraightFlush(List<Card> cardList)
+        {
+            return false;
+        }
+        public static bool IsFourKind(List<Card> cardList)
+        {
+            int iMaxSameNumCount = GetMaxSameNumCount(cardList);
+            return iMaxSameNumCount == 4;
+        }
+        public static bool IsThreeKind(List<Card> cardList)
+        {
+            return false;
+        }
+        public static bool IsFullHouse(List<Card> cardList)
+        {
+            return false;
+        }
+        public static bool IsTwoPairs(List<Card> cardList)
+        {
+            return false;
+        }
+        public static bool IsOnePairs(List<Card> cardList)
+        {
+            return false;
+        }
+        public static int GetMaxSameCardType(List<Card> cardList)
+        {
+            int iFour = 0;
+            int iThree = 0;
+            int iTwo = 0;
+            int iPairsCount = 0;
+
+            int iMaxSameNumCount = 0;
+            int[] arrCount = new int[14];
+
+            foreach (var item in cardList)
+            {
+                arrCount[item.iNumber] += 1;
+            }
+            for (int i = 1; i < arrCount.Length; i++)
+            {
+                var iCount = arrCount[i];
+                if (iCount == 4)
+                {
+                    iFour = i;
+                    break;
+                }
+                if (iCount == 3 && i > iThree)
+                {
+                    iThree = i;
+                }
+                else if (iCount == 2)
+                {
+                    iPairsCount++;
+                    if (i > iTwo)
+                    {
+                        iTwo = i;
+                    }
+                }
+            }
+
+            foreach (var item in arrCount)
+            {
+                if (item == 4)
+                {
+                    return CardType.FourKind;
+                }
+            }
+
+            if (iMaxSameNumCount == 4)
+            {
+                return CardType.FourKind;
+            }
+
+            return CardType.HighCard;
+        }
+
+        public static int GetCardType(List<Card> cardList)
+        {
+            int iMaxSameNumCount = 0;
+            int[] arrCount = new int[14];
+
+            foreach (var item in cardList)
+            {
+                arrCount[item.iNumber] += 1;
+            }
+
+            foreach (var item in arrCount)
+            {
+                if (item > iMaxSameNumCount)
+                {
+                    iMaxSameNumCount = item;
+                }
+            }
+
+            if (iMaxSameNumCount==4)
+            {
+                return CardType.FourKind;
+            }
+
+            return CardType.HighCard;
+        }
         public static List<Card> GetMaxCards(List<Card> srcCards)
         {
             List<Card> retCards = new List<Card>();
---
+++ Hunter/Hunter/TexasHoldem/TexasHoldemClass.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hunter
+{
+    public class Color
+    {
+        public const int Spade = 1;
+        public const int Heart = 2;
+        public const int Club = 3;
+        public const int Diamond = 4;
+        public const int Count = 4;
+    }
+
+    public class CardType
+    {
+        public static int Count = 100;
+        public static int StraightFlush = Count--;
+        public static int FourKind = Count--;
+        public static int FullHouse = Count--;
+        public static int Flush = Count--;
+        public static int Straight = Count--;
+        public static int ThreeKind = Count--;
+        public static int TwoPairs = Count--;
+        public static int OnePairs = Count--;
+        public static int HighCard = Count--;
+    }
+
+    public class MaxCardType
+    {
+        public int iCardType;
+        public List<Card> cardList;
+    }
+
+    public class Card
+    {
+        public static int iTotalCount = 52;
+        public int id;
+        public int iColor;
+        public int iNumber;
+        public bool bUsed;
+        public int GetId()
+        {
+            return (iColor - 1) * 13 + iNumber;
+        }
+        public static int CountId(int iColor, int iNumber)
+        {
+            return (iColor - 1) * 13 + iNumber;
+        }
+        public string Print()
+        {
+            string sColor = "";
+            string sNumber = "";
+            string ret = "";
+            switch (iColor)
+            {
+                case Color.Spade:
+                    sColor = "S";
+                    break;
+                case Color.Heart:
+                    sColor = "H";
+                    break;
+                case Color.Club:
+                    sColor = "C";
+                    break;
+                case Color.Diamond:
+                    sColor = "D";
+                    break;
+                default:
+                    break;
+            }
+            switch (iNumber)
+            {
+                case 1:
+                    sNumber = "A";
+                    break;
+                case 10:
+                    sNumber = "T";
+                    break;
+                case 11:
+                    sNumber = "J";
+                    break;
+                case 12:
+                    sNumber = "Q";
+                    break;
+                case 13:
+                    sNumber = "K";
+                    break;
+                default:
+                    sNumber = Convert.ToString(iNumber);
+                    break;
+            }
+            ret = sColor + sNumber;
+            return ret;
+        }
+    }
+}
