7d31367a8d9dadbb59838a364a463396a16e27bd
dingxiaocheng <dingxiaocheng@shinezone.com>
2018-03-02 18:01:52 +0800

    [CODE] daily commit

--- Hunter/Hunter/TexasHoldem/CardCompare.cs
+++ Hunter/Hunter/TexasHoldem/CardCompare.cs
@@ -16,8 +16,7 @@ namespace Hunter.TexasHoldem
         public int iCardType;
         public List<Card> cardList;
     }
-
-
+    
     public class CardTypeCaculator
     {
         public int iCardType;
@@ -25,9 +24,28 @@ namespace Hunter.TexasHoldem
         public List<Card> mMaxCardList;
         public int[] arrNumCount;
         public int[] arrColorCount;
+
+        //StraightFlush
+        //FourKind
+        //Flush
+        //Straight
+        //ThreeKind
+        //HighCard
         public int iHighCard;
-        public int iPairA;
-        public int iPairB;
+
+        public int iFullHouse_Three;
+        public int iFullHouse_Two;
+
+        public int iThreeKind_Three;
+        public int iThreeKind_High1;
+        public int iThreeKind_High2;
+
+        public int iTwoPair_Pair1;
+        public int iTwoPair_Pair2;
+        public int iTwoPair_High;
+
+        public int iOnePair_Pair;
+        public int iOnePair_High;
 
         public CardTypeCaculator(List<Card> _CardList)
         {
@@ -35,80 +53,12 @@ namespace Hunter.TexasHoldem
 
             iCardType = CardType.None;
             mCardList = _CardList;
-            mMaxCardList = null;
+            mMaxCardList = new List<Card>();
             arrNumCount = new int[14];
             arrColorCount = new int[5];
             iHighCard = 0;
         }
-
-        public void UpdateCardType(int _iCardType)
-        {
-            if (_iCardType > iCardType)
-            {
-                iCardType = _iCardType;
-            }
-            iHighCard = 0;
-        }
-        public void UpdateCardType(int _iCardType, int _iHighCard)
-        {
-            if (_iCardType > iCardType)
-            {
-                iCardType = _iCardType;
-            }
-            iHighCard = _iHighCard;
-        }
-
-        public void ProcessSameNumber()
-        {
-            int iThree = 0;
-            int iTwo = 0;
-            int iPairsCount = 0;
-
-            int iMaxSameNumCount = 0;
-            arrNumCount = new int[14];
-
-            foreach (var item in mCardList)
-            {
-                arrNumCount[item.iNumber] += 1;
-            }
-
-            for (int i = 1; i < arrNumCount.Length; i++)
-            {
-                var iCount = arrNumCount[i];
-                if (iCount == 4)
-                {
-                    UpdateCardType(CardType.FourKind, i);
-                    return;
-                }
-                if (iCount == 3 && i > iThree)
-                {
-                    iThree = i;
-                    UpdateCardType(CardType.FourKind, i);
-                }
-                else if (iCount == 2)
-                {
-                    iPairsCount++;
-                    if (i > iTwo)
-                    {
-                        iTwo = i;
-                    }
-                }
-            }
-
-            foreach (var item in arrNumCount)
-            {
-                if (item == 4)
-                {
-                    
-                }
-            }
-
-            if (iMaxSameNumCount == 4)
-            {
-            }
-
-            UpdateCardType(CardType.HighCard);
-        }
+        
         public int CalcCardType()//PreProcess
         {
             foreach (var item in mCardList)
@@ -152,32 +102,6 @@ namespace Hunter.TexasHoldem
 
             return CardType.HighCard;
         }
-        
-        public int CalcCardType(List<Card> cardList)
-        {
-            int iMaxSameNumCount = 0;
-            int[] arrCount = new int[14];
-
-            foreach (var item in cardList)
-            {
-                arrCount[item.iNumber] += 1;
-            }
-
-            foreach (var item in arrCount)
-            {
-                if (item > iMaxSameNumCount)
-                {
-                    iMaxSameNumCount = item;
-                }
-            }
-
-            if (iMaxSameNumCount == 4)
-            {
-                return CardType.FourKind;
-            }
-
-            return CardType.HighCard;
-        }
 
         public List<Card> GetMaxCards(List<Card> srcCards)
         {
@@ -197,11 +121,12 @@ namespace Hunter.TexasHoldem
 
         public bool IsFourKind()
         {
-            foreach (var count in arrNumCount)
+            for (int i = 1; i < arrNumCount.Length; i++)
             {
-                if (count == 4)
+                var iCount = arrNumCount[i];
+                if (iCount == 4)
                 {
-                    UpdateCardType(CardType.FourKind);
+                    iHighCard = i;
                     return true;
                 }
             }
@@ -210,24 +135,38 @@ namespace Hunter.TexasHoldem
 
         public bool IsFullHouse()
         {
-            int iThree = 0;
-            int iTwo = 0;
+            //  iFullHouse_Three;
+            //public int iFullHouse_Two;
+            iFullHouse_Three = 0;
+            iFullHouse_Two = 0;
             for (int i = 1; i < arrNumCount.Length; i++)
             {
                 var iCount = arrNumCount[i];
-                if (iCount == 3 && i > iThree)
+                if (iCount == 3 && i > iFullHouse_Three)
                 {
-                    iThree = i;
-                }
-                else if (iCount == 2 && i > iTwo)
-                {
-                    iTwo = i;
-                }
-                if (iThree > 0 && iTwo > 0)
-                {
-                    return true;
+                    iFullHouse_Three = i;
                 }
             }
+
+            for (int i = 1; i < arrNumCount.Length; i++)
+            {
+                if (i == iFullHouse_Three)
+                {
+                    continue;
+                }
+
+                var iCount = arrNumCount[i];
+                if (iCount >= 2 && i > iFullHouse_Two)
+                {
+                    iFullHouse_Two = i;
+                }
+            }
+
+            if (iFullHouse_Three > 0 && iFullHouse_Two > 0)
+            {
+                return true;
+            }
+
             return false;
         }
 
@@ -265,11 +204,12 @@ namespace Hunter.TexasHoldem
         
         public bool IsThreeKind()
         {
-            foreach (var count in arrNumCount)
+            int iThree = 0;
+            for (int i = 1; i < arrNumCount.Length; i++)
             {
-                if (count == 2)
+                var iCount = arrNumCount[i];
+                if (iCount == 3)
                 {
-                    UpdateCardType(CardType.FourKind);
                     return true;
                 }
             }
@@ -278,11 +218,39 @@ namespace Hunter.TexasHoldem
 
         public bool IsTwoPairs()
         {
+            int iBigPairs = 0;
+            int iSmallPairs = 0;
+            for (int i = arrNumCount.Length - 1; i > 0 ; i++)
+            {
+                var iCount = arrNumCount[i];
+                if (iCount == 2)
+                {
+                    if (iBigPairs == 0)
+                    {
+                        iBigPairs = i;
+                        continue;
+                    }
+                    else if (iSmallPairs == 0)
+                    {
+                        iSmallPairs = i;
+                        continue;
+                    }
+                    return true;
+                }
+            }
             return false;
         }
 
         public bool IsOnePairs()
         {
+            for (int i = 1; i < arrNumCount.Length; i++)
+            {
+                var iCount = arrNumCount[i];
+                if (iCount == 2)
+                {
+                    return true;
+                }
+            }
             return false;
         }
 
